22bbecefc1a8f4caf220849dd43a87ebac26f4a3
Chowiee <165966007+Chiwiee@users.noreply.github.com>
2024-06-09 12:42:02 +0800

    Add logic/pseudocode

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log("Hello, World!");
+//console.log("Hello, World!");
+
+// getComputerChoice logic/algorithm
+// 1. Create a new function named getComputerChoice
+// 2. Write the code so that getComputerChoice will randomly return one of the following string values: “rock”, “paper” or “scissors”.
+// 3. Test what your function returns by using console.log.
+
+// getHumanChoice logic/algorithm
+// 1. Create a new function named getHumanChoice.
+// 2. Write the code so that getHumanChoice will return one of the valid choices depending on what the user inputs.
+// 3. Test what your function returns by using console.log.
+
+// Declare the players score variables
+// 1. Create two new variables named humanScore and computerScore in the global scope.
+// 2. Initialize those variables with the value of 0.
+
+// Write the logic to play a single round
+// 1. Create a new function named playRound.
+// 2. Define two parameters for playRound: humanChoice and computerChoice. Use these two parameters to take the human and computer choices as arguments.
+// 3. Make your function’s humanChoice parameter case-insensitive so that players can input “rock”, “ROCK”, “RocK”, or other variations.
+// 4. Write the code for your playRound function to console.log a string value representing the round winner, such as: “You lose! Paper beats Rock”.
+// 5. Increment the humanScore or computerScore variable based on the round winner.
+
+// Write the logic to play the entire game
+// 1. Create a new function named playGame.
+// 2. Move your playRound function and score variables so that they’re declared inside of the new playGame function
+// 3. Play 5 rounds by calling playRound 5 times.
