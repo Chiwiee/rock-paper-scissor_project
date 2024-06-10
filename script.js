d6909b6ba9121a8f9b788e080a3471587a83c961
Chowiee <165966007+Chiwiee@users.noreply.github.com>
2024-06-10 12:05:05 +0800

    Remove choice variable

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 // Step 1
 function getComputerChoice() {
   const result = Math.floor(Math.random() * 3) + 1;
-  let choice;
   if (result === 1) {
     choice = "Rock";
   } else if (result === 2) {
