be54784ef4758ff563c24926fba5420566a1d57b
Chowiee <165966007+Chiwiee@users.noreply.github.com>
2024-06-10 12:05:42 +0800

    Change if variable to return

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 function getComputerChoice() {
   const result = Math.floor(Math.random() * 3) + 1;
   if (result === 1) {
-    choice = "Rock";
+    return "Rock";
   } else if (result === 2) {
     choice = "Paper";
   } else {
