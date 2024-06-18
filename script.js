436074a79fd43d38a50f6696924b3153b180eb01
Chowiee <165966007+Chiwiee@users.noreply.github.com>
2024-06-18 22:52:48 +0800

    Move playGame inside of EventListener

--- script.js
+++ script.js
@@ -40,56 +40,62 @@ let round = 1;
 
 const button = document.querySelectorAll("button");
 button.forEach((button) => {
-  button.addEventListener("click", function (e) {});
-});
-function playGame() {
-  function playRound(humanChoice, computerChoice) {
-    if (humanChoice === computerChoice) {
-      tieScore += 1;
-      round += 1;
-      return "Draw!";
-    } else if (humanChoice === "rock" && computerChoice === "paper") {
-      computerScore += 1;
-      round += 1;
-      return "You Lose! Paper beats Rock";
-    } else if (humanChoice === "paper" && computerChoice === "scissor") {
-      computerScore += 1;
-      round += 1;
-      return "You Lose! Scissor beats Paper";
-    } else if (humanChoice === "scissor" && computerChoice === "rock") {
-      computerScore += 1;
-      round += 1;
-      return "You Lose! Rock beats Scissor";
-    } else {
-      humanScore += 1;
-      round += 1;
-      return "You Win!";
+  button.addEventListener("click", function (e) {
+    playerSelection = button.id;
+
+    function playGame() {
+      function playRound(humanChoice, computerChoice) {
+        if (humanChoice === computerChoice) {
+          tieScore += 1;
+          round += 1;
+          return "Draw!";
+        } else if (humanChoice === "rock" && computerChoice === "paper") {
+          computerScore += 1;
+          round += 1;
+          return "You Lose! Paper beats Rock";
+        } else if (humanChoice === "paper" && computerChoice === "scissor") {
+          computerScore += 1;
+          round += 1;
+          return "You Lose! Scissor beats Paper";
+        } else if (humanChoice === "scissor" && computerChoice === "rock") {
+          computerScore += 1;
+          round += 1;
+          return "You Lose! Rock beats Scissor";
+        } else {
+          humanScore += 1;
+          round += 1;
+          return "You Win!";
+        }
+      }
+      // Players Selection
+      const humanChoice = getHumanChoice();
+      console.log(humanChoice);
+      const computerChoice = getComputerChoice();
+
+      // Scoring, Tie Count and Game Result
+      const result = document.querySelector("#result");
+      result.textContent = `Game Result: ${playRound(
+        humanChoice,
+        computerChoice
+      )}`;
+
+      const computerPick = document.querySelector("#computer-pick");
+      computerPick.textContent = `Computer Pick: ${computerChoice}`;
+
+      const playerScore = document.querySelector("#player-score");
+      playerScore.textContent = `Player Score: ${humanScore}`;
+
+      const botScore = document.querySelector("#computer-score");
+      botScore.textContent = `Computer Score: ${computerScore}`;
+
+      const tieCount = document.querySelector("#tie-count");
+      tieCount.textContent = `Tie: ${tieScore}`;
+
+      const roundCount = document.querySelector("#round");
+      roundCount.textContent = `Round: ${round}`;
     }
-  }
-  // Players Selection
-  const humanChoice = getHumanChoice();
-  console.log(humanChoice);
-  const computerChoice = getComputerChoice();
-
-  // Scoring, Tie Count and Game Result
-  const result = document.querySelector("#result");
-  result.textContent = `Game Result: ${playRound(humanChoice, computerChoice)}`;
-
-  const computerPick = document.querySelector("#computer-pick");
-  computerPick.textContent = `Computer Pick: ${computerChoice}`;
-
-  const playerScore = document.querySelector("#player-score");
-  playerScore.textContent = `Player Score: ${humanScore}`;
-
-  const botScore = document.querySelector("#computer-score");
-  botScore.textContent = `Computer Score: ${computerScore}`;
-
-  const tieCount = document.querySelector("#tie-count");
-  tieCount.textContent = `Tie: ${tieScore}`;
-
-  const roundCount = document.querySelector("#round");
-  roundCount.textContent = `Round: ${round}`;
-}
+  });
+});
 
 // // Play
 // const rockbtn = document.querySelector("#pickRock");
